9e20d0640407e0921fe77ccc1cbbde4450a0363d
Mustafa Yücel <mustafayucel@gmail.com>
2018-09-24 14:37:20 +0300

    Create Sample.cs

---
+++ Sample.cs
@@ -0,0 +1,17 @@
+public class Startup
+{
+    public Startup()
+    {
+        Configuration = configuration;
+        var list = new List<int> { 1, 2, 3, 4 };
+        var pageHelper = new PageHelper(list.Count, 1);
+        while (pageHelper.NextPage())
+        {
+            Debug.WriteLine("Page: " + pageHelper.CurrentPage);
+            while (pageHelper.NextItem())
+            {
+                Debug.WriteLine("Record: " + pageHelper.CurrentRecord);
+            }
+        }
+    }
+}
