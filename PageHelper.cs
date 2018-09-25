8f900a7d8d2221ce5a1fdf3e9d279a778ac38406
Mustafa Yücel <mustafayucel@gmail.com>
2018-09-25 10:27:31 +0300

    Update PageHelper.cs

--- PageHelper.cs
+++ PageHelper.cs
@@ -24,7 +24,7 @@
 
         public bool NextPage()
         {
-            if (PageCount <= CurrentPage)
+            if (CurrentPage >= PageCount)
                 return false;
 
             CurrentPage++;
